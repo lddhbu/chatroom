5223eab7889741f4dfa3a52565104ddad31e722a
Administrator <ldd@ubuntu.ubuntu-domain>
2013-03-01 17:32:37 +0800

    添加字符串相似度算法

--- chat.py
+++ chat.py
@@ -9,6 +9,33 @@ import tornado.web
 from base import BaseHandler
 import tornado.escape
 
+_SPAM_LEN_LIMIT=5
+_SPAM_SIM_LIMIT=2.0
+
+def similar_degrees(s1,s2):
+    """
+    检查两个字符串的相似度，抽象为编辑距离问题，基于动态规划思想，时间复杂度为O(mn)，空间复杂度为O(n)。
+    相似度和“编辑距离”成反相关，和字符串的长度成正相关，这里我简单的认为相似度=“长度”/“编辑距离“(这里的长度为m和n较长者)。
+    d[0]表示上次最优结果，d[1]表示这次最优结果
+    """
+    m=len(s1)
+    n=len(s2)
+    d=[[i for i in range(n+1)] for j in [0,1]]
+    for i in range(1,m+1):
+        d[0][0]=i-1#边界条件，上一次
+        d[1][0]=i#边界条件，本次
+        for j in range(1,n+1):
+            cost=(s1[i-1]!=s2[j-1] and 1 or 0)+d[0][j-1]#替换
+            temp=cost>d[1][j-1]+1 and d[1][j-1]+1 or cost#插入
+            d[1][j]=temp>d[0][j]+1 and d[0][j]+1 or temp#插入
+        for j in range(1,n+1):
+            d[0][j]=d[1][j]
+
+    length=d[1][n]#编辑距离
+    ret=max(m,n)/(length+1.0)
+    return ret
+
+
 class ChatRoomHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
@@ -39,6 +66,10 @@ class MessageMixin(object):
         for client in cls.clients:
             client.send(json.dumps({'type':3,'time':now,'user':name,'message':message}))
 
+    def is_spam(self):
+        """
+        有两个限制，一是字符串长度，二是和上一次的发言的相似度
+        """
 
 
 class SocketHandler(sockjs.tornado.SockJSConnection,MessageMixin):
