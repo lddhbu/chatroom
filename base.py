933f8e4e9553217027a1db0dc6a1d4b5cda5dbc7
LongWind <lddhbu@gmail.com>
2013-04-15 08:55:45 +0800

    some style

--- base.py
+++ base.py
@@ -1,12 +1,14 @@
 import tornado.web
 import tornado.escape
 
+
 class BaseHandler(tornado.web.RequestHandler):
 	def get_current_user(self):
 		user_json=self.get_secure_cookie("user")
 		if not user_json:
 			return None
 		return tornado.escape.json_decode(user_json)
+
 
 class IndexHandler(BaseHandler):
 	def get(self):
